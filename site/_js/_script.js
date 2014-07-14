5c8dbd9f9e62c8d1f4f6d24d21cd47bc29c37950
Albin Rodriguez <ealbinu@gmail.com>
2014-07-14 02:15:46 -0500

    updated

---
+++ site/_js/_script.js
@@ -0,0 +1,20 @@
+require.config({
+    baseUrl: '_site/_js/',
+    paths: {
+        jquery: 'jquery',
+        angular: 'angular'
+    }
+});
+
+
+
+// Document Ready
+require(['jquery', 'angular'], function( $ ) {
+    
+    
+    
+    
+    
+    
+}); // document.ready
+
